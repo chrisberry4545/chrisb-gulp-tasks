4d30ccafbd91a6087e6acf81cc880355971e406d
Christopher <chrisb@Christophers-MacBook-Air.local>
2017-05-30 11:10:36 +0100

    Fixed error in build process

--- tasks/gulp.build-dev.js
+++ tasks/gulp.build-dev.js
@@ -3,10 +3,9 @@ const runSequence = require('run-sequence');
 
 module.exports = (config) => {
     gulp.task('build-dev', (cb) => {
-        config.buildTasksDev.push(cb);
         runSequence.apply(
             null,
-            config.buildTasksDev
+            [].concat(config.buildTasksDev, cb)
         );
     });
 };
--- tasks/gulp.build-dist.js
+++ tasks/gulp.build-dist.js
@@ -3,10 +3,9 @@ const runSequence = require('run-sequence');
 
 module.exports = (config) => {
     gulp.task('build-dist', (cb) => {
-        config.buildTasksDist.push(cb);
         runSequence.apply(
             null,
-            config.buildTasksDev
+            [].concat(config.buildTasksDist, cb)
         );
     });
 };
